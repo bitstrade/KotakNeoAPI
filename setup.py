d81c31a3468a21658f70e278e189041a6a32bde4
Bits Trade <tech@bitstrade.in>
2024-09-25 00:23:55 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 NAME = "neo-api-client"
-VERSION = "1.2.0"
+VERSION = "1.2.1"
 # To install the library, run the following
 #
 # python setup.py install
